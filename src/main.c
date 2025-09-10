51f0c98dbe861e7864aaa0aff68f215bc05eff12
Yuxin Wu <yuwu@c2r5p13.hive.fi>
2025-09-10 13:58:27 +0300

    main for testing mlx

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 // #inlcude "minirt.h"
+//#include "so_long.h"
 
 //remove to .h later
 #include <fcntl.h>
@@ -15,20 +16,29 @@
 //test version for compiling
 int main(int ac, char **av)
 {
-    if (ac != 2)
-        return (-1);
+	if (ac != 2)
+		return (-1);
 
     //test get_next_line
-    int fd = open(av[1], O_RDONLY);
-    while (1)
-    {
-        char   *line = get_next_line(fd);
-        printf("%s\n", line);
-        free (line);
-    }
-    
-    //test mlxlib
-    mlx_init(720, 480, "miniRT_test", true);
+	int fd = open(av[1], O_RDONLY);
+	char   *line;
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s\n", line);
+		free (line);
+		line = get_next_line(fd);
+	}
 
-    return (0);
+	//test mlxlib
+	mlx_t		*mlx_window;
+    mlx_image_t *mlx_image;
+
+	mlx_window = mlx_init(720, 480, "miniRT_test", false);
+	mlx_image = mlx_new_image(mlx_window, 480, 720);
+	mlx_image_to_window(mlx_window, mlx_image, 0, 0);	//把 image 显示到窗口	知道它是怎么把画布放上去的
+	// mlx_put_pixel(image, x, y, color)	    //在 image 上画像素	学会怎么画一个点
+	mlx_loop(mlx_window);                       	
+	mlx_terminate(mlx_window);
+	return (0);
 }
